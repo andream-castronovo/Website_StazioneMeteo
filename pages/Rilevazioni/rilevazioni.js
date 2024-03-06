3e25bc4dc8c46f5de067e6a32d26747a0bebf3ba
andream-castronovo <andreamaria.castronovo.stud@ispascalcomandini.it>
2024-03-06 09:48:33 +0100

    js

---
+++ pages/Rilevazioni/rilevazioni.js
@@ -0,0 +1,22 @@
+const staz = document.getElementById('stazioni')
+
+opzioni = [
+
+]
+
+asd = ['Cesena','Santarcangelo','Forlì','Faenza']
+
+asd.forEach((e) => {
+    o = document.createElement("option")
+    o.value = e
+    o.createTextNode(e + "")
+    
+    opzioni.appendChild(
+        
+    )
+})
+
+staz.appendChild(
+    
+)
+
