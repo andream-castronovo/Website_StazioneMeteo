290cf1873032e38da91e793d62ee26c76e65136d
andream-castronovo <andreamaria.castronovo.stud@ispascalcomandini.it>
2024-03-13 11:47:10 +0100

    Commit automatico consegna 2024-03-13

--- pages/Rilevazioni/rilevazioni.js
+++ pages/Rilevazioni/rilevazioni.js
@@ -1,22 +1,51 @@
 const staz = document.getElementById('stazioni')
+const sens = document.getElementById('sensori')
 
-opzioni = [
+const connectionString =  "Server=PC0128;Database=meteodb;Uid=ServerMeteoDBPascal;Pwd=MeteoPascalReading;";
 
-]
+const queryLocation = "SELECT DISTINCT Localita_Indirizzo FROM stazioni;"
+localita = ['Cesena','Santarcangelo','Forlì','Faenza'] // SELECT DISTINCT Localita_Indirizzo FROM Stazioni;
 
-asd = ['Cesena','Santarcangelo','Forlì','Faenza']
-
-asd.forEach((e) => {
+localita.forEach((e) => {
     o = document.createElement("option")
     o.value = e
-    o.createTextNode(e + "")
+    o.appendChild(document.createTextNode(e))
     
-    opzioni.appendChild(
-        
+    staz.append(
+        o
     )
 })
 
-staz.appendChild(
+const querySensors = "SELECT DISTINCT GrandezzaFisica FROM grandezzafisica;"
+sensori = ['Temperatura','Umidità','Pressione','Velocità pioggia'] // SELECT DISTINCT GrandezzaFisica FROM GrandezzaFisica;
+
+sensori.forEach((e) => {
+    o = document.createElement("option")
+    o.value = e
+    o.appendChild(document.createTextNode(e))
     
-)
+    sens.append(
+        o
+    )
+})
+
+data = fetch("./example.json")
+srcBarKeydown = () => {
+    console.log(data)
+}
+
+
+/*
+
+SELECT 
+    GrandezzaFisica.GrandezzaFisica, Rilevamenti.Dato,
+    Stazioni.Localita_Indirizzo, Rilevamenti.DataOra
+FROM Rilevamenti INNER JOIN
+     SensoriInstallati ON SensoriInstallati.idSensoriInstallati = Rilevamenti.idSensoriInstallati INNER JOIN
+     Stazioni ON Stazioni.idNomeStazione = SensoriInstallati.idNomeStazione INNER JOIN
+     Sensori ON Sensori.idCodiceSensore = SensoriInstallati.idCodiceSensore INNER JOIN
+     GrandezzaFisica ON GrandezzaFisica.idGrandezzaFisica = Sensori.idGrandezzaFisica
+WHERE GrandezzaFisica.GrandezzaFisica = {0} AND Stazioni.Localita_Indirizzo = {1};
+
+*/
 
